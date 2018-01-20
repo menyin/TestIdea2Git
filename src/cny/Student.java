a46aef709750654be3bd9de3c2d1f0a824974cae
menyin <845257580@qq.com>
2018-01-20 23:38:03 +0800

    学习方法中增加了人生路漫漫2

--- src/cny/Student.java
+++ src/cny/Student.java
@@ -6,5 +6,7 @@ public class Student {
         System.out.println("上晚自习");
         System.out.println("周末加班");
         System.out.println("人生路漫漫，谁与我相伴");
+        System.out.println("人生路漫漫，谁与我相伴");
+
     }
 }
